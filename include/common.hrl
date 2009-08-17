7ef3eae2e49d95bf2d0b8327c10a42328d4c3429
Davide Pesavento <davidepesa@gmail.com>
2009-08-17 23:58:22 +0200

    [common] Implement a new macro for debugging purposes.

--- include/common.hrl
+++ include/common.hrl
@@ -1,2 +1,8 @@
 -define(ERR(Msg), io:format("[~s] ERROR: ~p~n", [?MODULE_STRING, Msg])).
 -define(WARN(Msg), io:format("[~s] WARNING: ~p~n", [?MODULE_STRING, Msg])).
+
+-ifdef(debug).
+-define(DBG(Msg), io:format("[~s] ~p~n", [?MODULE_STRING, Msg])).
+-else.
+-define(DBG(Msg), true).
+-endif.
