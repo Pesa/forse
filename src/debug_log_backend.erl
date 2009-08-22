942c05dddc95f07022a8ce8398d9ca7422427604
Daniele Battaglia <dbat.fk@gmail.com>
2009-08-22 12:49:00 +0200

    [debug_log_backend] Preliminary version. [event_dispatcher] Preliminary version. [race_info_backend] Preliminary version. [team_backend] Preliminary version. [weather_backend] Preliminary version.

---
+++ src/debug_log_backend.erl
@@ -0,0 +1,124 @@
+-module(debug_log_backend).
+
+-behaviour(gen_server).
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% External exports
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1,
+		 handle_call/3,
+		 handle_cast/2,
+		 handle_info/2,
+		 terminate/2,
+		 code_change/3]).
+
+-define(GLOBAL_NAME, {global, ?MODULE}).
+-define(LOCAL_NAME, {local, ?MODULE}).
+
+-record(state, {observers}).
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+start_link() ->
+	gen_server:start_link(?LOCAL_NAME, ?MODULE, [], []).
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+	%TODO read observers list from mnesia in case of crash
+    {ok, #state{observers = []}}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast({subscribe, From}, State) ->
+	%TODO add subscriber in mnesia
+	OldObs = State#state.observers,
+	NewState = State#state{observers = [From | OldObs]}, 
+    {noreply, NewState};
+
+handle_cast({pitstop_notification, _Args}, State) ->
+	%TODO togliere l'entry nel journal
+	%TODO elaborare i dati ricevuti
+	{noreply, State};
+
+handle_cast({chrono_notification, _Args}, State) ->
+	%TODO togliere l'entry nel journal
+	%TODO elaborare i dati ricevuti
+	{noreply, State};
+
+handle_cast({surpass_notification, _Args}, State) ->
+	%TODO togliere l'entry nel journal
+	%TODO elaborare i dati ricevuti
+	{noreply, State};
+
+handle_cast({weather_notification, _Args}, State) ->
+	%TODO togliere l'entry nel journal
+	%TODO elaborare i dati ricevuti
+	{noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+
---
+++ src/event_dispatcher.erl
@@ -0,0 +1,167 @@
+-module(event_dispatcher).
+
+-behaviour(gen_server).
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% External exports
+-export([start_link/0, 
+		 subscribe/1,
+		 log/1]).
+
+%% gen_server callbacks
+-export([init/1,
+		 handle_call/3,
+		 handle_cast/2,
+		 handle_info/2,
+		 terminate/2,
+		 code_change/3]).
+
+-define(GLOBAL_NAME, {global, ?MODULE}).
+-define(PITSTOP_OBS, [debug_log_backend, team_backend, race_info_backend]).
+-define(CHRONO_OBS, [debug_log_backend, team_backend, race_info_backend]).
+-define(SURPASS_OBS, [debug_log_backend, team_backend, race_info_backend]).
+-define(WEATHER_OBS, [debug_log_backend, weather_backend, team_backend, race_info_backend]).
+
+-record(state, {}).
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+start_link() ->
+	gen_server:start_link(?GLOBAL_NAME, ?MODULE, [], []).
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+    {ok, #state{}}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+
+% Subscription message
+
+handle_call({subscribe, Service}, From, State) ->
+    % aggiunge il parametro From al 
+	% backend indicato in Service
+	gen_server:cast(service_map(Service), {subscribe, From}),
+    {reply, ok, State};
+
+% Race messages
+
+handle_call({pitstop_notification, Args}, _From, State) ->
+	internal_dispatching({pitstop_notification, Args}, ?PITSTOP_OBS),
+	{reply, ok, State};
+
+handle_call({chrono_notification, Args}, _From, State) ->
+	internal_dispatching({chrono_notification, Args}, ?CHRONO_OBS),
+	{reply, ok, State};
+
+handle_call({surpass_notification, Args}, _From, State) ->
+	internal_dispatching({surpass_notification, Args}, ?SURPASS_OBS),
+	{reply, ok, State};
+
+handle_call({weather_notification, Args}, _From, State) ->
+	internal_dispatching({weather_notification, Args}, ?WEATHER_OBS),
+	{reply, ok, State}.
+
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% Func: service_map/1
+%% Purpose: Map from services to dispatcher backends
+%% Returns: atom
+%% --------------------------------------------------------------------
+service_map(Service) ->
+	case Service of
+		debug_log -> debug_log_backend;
+		weather -> weather_backend;
+		team -> team_backend;
+		rece_info -> race_info_backend;
+		_ -> not_found
+	end.
+
+%% --------------------------------------------------------------------
+%% Func: internal_dispatching/2
+%% Purpose: Casts Msg to all process in list
+%% Returns: atom
+%% --------------------------------------------------------------------
+
+internal_dispatching(Msg, [Head | Tail]) ->
+	gen_server:cast(Head, Msg),
+	internal_dispatching(Msg, Tail);
+
+internal_dispatching(_Msg, []) ->
+	ok.
+
+%% --------------------------------------------------------------------
+%% Func: subscribe/1
+%% Purpose: Wraps
+%% Returns: atom
+%% --------------------------------------------------------------------
+
+subscribe(Service) ->
+	gen_server:call(?GLOBAL_NAME, {subscribe, Service}, infinity).
+
+log(Msg) ->
+	gen:server_call(?GLOBAL_NAME, Msg, infinity).
