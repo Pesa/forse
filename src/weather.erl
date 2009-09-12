e84ee7b549bbffbe925e6cda861ff1899f65cf18
Davide Pesavento <davidepesa@gmail.com>
2009-09-12 18:46:33 +0200

    [weather] New weather control module.

---
+++ src/weather.erl
@@ -0,0 +1,116 @@
+-module(weather).
+
+-behaviour(gen_server).
+
+%% External exports
+-export([start_link/0,
+		 apply_change/3,
+		 schedule_change/3]).
+
+%% gen_server callbacks
+-export([init/1,
+		 handle_call/3,
+		 handle_cast/2,
+		 handle_info/2,
+		 terminate/2,
+		 code_change/3]).
+
+-include("common.hrl").
+
+-record(state, {}).
+
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+start_link() ->
+	gen_server:start_link(?GLOBAL_NAME, ?MODULE, [], []).
+
+apply_change(_Time, NewWeather, Where) ->
+	gen_server:call(?GLOBAL_NAME, {apply_change, NewWeather, Where}, infinity).
+
+schedule_change(NewWeather, Where, When) ->
+	gen_server:call(?GLOBAL_NAME, {schedule_change, NewWeather, Where, When}).
+
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+	% TODO: setup the pre-configured weather changes.
+    {ok, #state{}}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call({apply_change, NewWeather, Where}, _From, State) ->
+	% TODO: apply the changes in mnesia.
+	{reply, done, State};
+
+handle_call({schedule_change, NewWeather, Where, When}, _From, State) ->
+	Reply = scheduler:queue_work(When, {?MODULE, apply_change, [NewWeather, Where]}),
+	{reply, Reply, State};
+
+handle_call(Msg, From, State) ->
+	?WARN({"unhandled call", Msg, "from", From}),
+	{noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(Msg, State) ->
+	?WARN({"unhandled cast", Msg}),
+	{noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(Msg, State) ->
+	?WARN({"unhandled info", Msg}),
+	{noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+	ok.
+
+%% --------------------------------------------------------------------
+%% Function: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+
+%% --------------------------------------------------------------------
+%% Internal functions
+%% --------------------------------------------------------------------
