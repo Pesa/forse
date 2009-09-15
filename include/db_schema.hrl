2e0ccc5ef169f164c3f4d22e5994b3deecbc0fa4
Davide Pesavento <davidepesa@gmail.com>
2009-09-15 17:19:11 +0200

    [db_schema] Added 'run_preelab' field to pilot.

--- include/db_schema.hrl
+++ include/db_schema.hrl
@@ -6,15 +6,14 @@
 
 -define(TRACK_TAB, track).
 
-%%------------------------
+%% ------------------------
 %% car_id: ref to pilot table
-%% enter_t: the time in which the car entered in the current segment 
-%% 			(the time it exited from the previous one)
-%% exit_t: the timein which the car will exit from the current segment
+%% enter_t: when the car entered the current segment
+%% 			(i.e. the time it exited from the previous one)
+%% exit_t: when the car will leave the current segment
 %% speed: exit speed
 %% lane: exit lane
-%%------------------------
-
+%% ------------------------
 -record(car_position,{car_id,
 					  enter_t,
 					  exit_t,
@@ -22,19 +21,18 @@
 					  enter_lane,
 					  exit_lane}).
 
-%%------------------------
+%% ------------------------
 %% id: unique numerical identifier
 %% type: straight | pitstop | pitlane | bent | intermediate
-%% TODO il traguardo e le postazioni di partenza?
+%% TODO: il traguardo e le postazioni di partenza?
 %% min_lane: minimum lane index
 %% max_lane: maximum lane index
-%% lenght: the lenght of the segment
-%% inclination: degrees
+%% length: the length of the segment
+%% inclination: in degrees
 %% rain: integer from 0 (sun) to 10 (heavy rain)
-%% curvature: guess..
-%% queued_cars: list of car_position
+%% curvature: radius of curvature
+%% queued_cars: list of car_position records
 %% -----------------------
-
 -record(segment,{id,
 				 type,
 				 min_lane,
@@ -45,44 +43,42 @@
 				 curvature,
 				 queued_cars = []}).
 
-%%------------------------
+%% ------------------------
 %% id: unique numerical identifier
 %% name: pilot's name
-%% segment: segment.id pointing to the pilot's actual position
 %% skill: integer representing pilot's skill
 %% weight: pilot's weight
-%% car_status: tyres and fuel
+%% car_status: current car status
+%% segment: segment.id pointing to the pilot's current position
 %% team_name: reference to car_type tab
+%% run_preelab: set to true when the preelaboration phase must be re-run
 %% -----------------------
-
 -record(pilot,{id,
 			   name,
-			   segment,
 			   skill,
 			   weight,
-			   car_status,
-			   team_name}).
+			   car_status = #car_status{},
+			   segment,
+			   team_name,
+			   run_preelab = true}).
 
-%%------------------------
+%% ------------------------
 %% smg_id: segment id
-%% bound: max speed calculated at preelaboration time
-%%------------------------
-
+%% bound: maximum speed calculated at preelaboration time
+%% ------------------------
 -record(speed_bound, {sgm_id,
 					  bound}).
 
-%%------------------------
-%% id: unique numerical identifier
+%% ------------------------
+%% team_name: name of the team to which the car belongs
 %% brake: determines max deceleration (must be negative)
 %% power: determines max speed and max acceleration
-%% weight: car's weight (fuel not included)
-%% team_name: guess...
+%% weight: car's weight (excluding fuel)
 %% -----------------------
-
 -record(car_type,{team_name,
 				  brake,
 				  power,
 				  weight}).
 
--record(setting, {key,
-				  value}).
+%% ------------------------
+-record(setting, {key, value}).
