52dbbe0cf6be2873466381b07853a374ea99f105
Davide Pesavento <davidepesa@gmail.com>
2009-09-23 23:16:19 +0200

    [access] Optimize a bit more.

--- src/access.erl
+++ src/access.erl
@@ -1,6 +1,6 @@
 -module(access).
 
-%% Exported Functions
+%% Exported functions
 -export([allow_move/5]).
 
 %% Include files
@@ -18,32 +18,28 @@ allow_move(Pilot, Sgm, EnterLane, ExitLane, Pit) when is_record(Pilot, pilot),
 	MaxL = Sgm#segment.max_lane,
 	MinL = Sgm#segment.min_lane,
 	Type = Sgm#segment.type,
-	
 	PS = Type == pitstop andalso EnterLane == MaxL - 1,
 	PL = Type == pitlane andalso EnterLane == MaxL,
 	PrePL = Type == pre_pitlane andalso EnterLane == MaxL,
 	PostPL = Type == post_pitlane andalso EnterLane == MaxL,
-	% TODO: si riesce a ottimizzare meglio?
-	%		(per evitare la lettura dal DB)
-	OwnPits = if
-				  PS ->
-					  T = utils:mnesia_read(car_type, Pilot#pilot.team_name),
-					  T#car_type.pitstop_sgm == Sgm#segment.id;
-				  true ->
-					  unused
-			  end,
-	
 	if
 		ExitLane < MinL;
 		ExitLane > MaxL;
 		abs(ExitLane - EnterLane) > 1;
 		PL andalso ExitLane /= EnterLane;
 		Type == pre_pitlane andalso ExitLane == MaxL andalso not Pit;
-		ExitLane < EnterLane andalso (PrePL orelse PostPL orelse PS);
-		PS andalso ExitLane == MaxL andalso not OwnPits;
-		PS andalso ExitLane == MaxL - 1 andalso OwnPits; 
-		Type == pitlane andalso EnterLane /= MaxL andalso ExitLane == MaxL; 
-		Type == pitstop andalso EnterLane < MaxL - 1 andalso ExitLane >= MaxL - 1 -> crash;
-		PS andalso ExitLane == MaxL andalso OwnPits -> pits;
+		Type == pitlane andalso EnterLane /= MaxL andalso ExitLane == MaxL;
+		Type == pitstop andalso EnterLane < MaxL - 1 andalso ExitLane >= MaxL - 1;
+		ExitLane < EnterLane andalso (PrePL orelse PostPL orelse PS) ->
+			crash;
+		PS ->
+			T = utils:mnesia_read(car_type, Pilot#pilot.team_name),
+			OwnPits = T#car_type.pitstop_sgm == Sgm#segment.id,
+			if
+				ExitLane == MaxL andalso not OwnPits;
+				ExitLane == MaxL - 1 andalso OwnPits -> crash;
+				ExitLane == MaxL andalso OwnPits -> pits;
+				true -> run
+			end;
 		true -> run
 	end.
