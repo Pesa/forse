672f722a9cce50026689f7e9963a9351b6fd6dc0
Davide Pesavento <davidepesa@gmail.com>
2009-09-23 18:27:37 +0200

    [dispatcher_sup] Make children permanent instead of transient.

--- src/dispatcher_sup.erl
+++ src/dispatcher_sup.erl
@@ -8,15 +8,13 @@
 %% supervisor callbacks
 -export([init/1]).
 
--include("common.hrl").
-
 
 %% ====================================================================
 %% External functions
 %% ====================================================================
 
 start_link() ->
-	supervisor:start_link(?GLOBAL_NAME, ?MODULE, []).
+	supervisor:start_link(?MODULE, []).
 
 
 %% ====================================================================
@@ -32,24 +30,24 @@ start_link() ->
 init([]) ->
 	Dispatcher = {event_dispatcher,
 				  {event_dispatcher, start_link, []},
-				  transient, 5000, worker,
+				  permanent, 5000, worker,
 				  [event_dispatcher]},
 	Backends = [
 				{debug_log,
 				 {debug_log_backend, start_link, []},
-				 transient, 5000, worker,
+				 permanent, 5000, worker,
 				 [debug_log_backend]},
 				{race_info,
 				 {race_info_backend, start_link, []},
-				 transient, 5000, worker,
+				 permanent, 5000, worker,
 				 [race_info_backend]},
 				{team,
 				 {team_backend, start_link, []},
-				 transient, 5000, worker,
+				 permanent, 5000, worker,
 				 [team_backend]},
 				{weather,
 				 {weather_backend, start_link, []},
-				 transient, 5000, worker,
+				 permanent, 5000, worker,
 				 [weather_backend]}
 			   ],
 	{ok, {{one_for_one, 20, 30},
