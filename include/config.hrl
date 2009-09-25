4de05f55e7c7d74cc6352a073dd2e508f2badd44
Davide Pesavento <davidepesa@gmail.com>
2009-09-25 19:05:53 +0200

    [config] Added SEGMENT_LENGTH.

--- include/config.hrl
+++ include/config.hrl
@@ -1,38 +1,38 @@
-%% -----------------------
-%%  Configuration parameters macros
-%% -----------------------
--include("db_schema.hrl").
-%% Time in milliseconds a car needs to move 
-%% to the adjacent lane
--define(LANE_CHANGE_TIME, 500).
+%%% ==========================
+%%%  Configuration parameters
+%%% ==========================
 
-%% sgm_number: number of segments in track
+-include("db_schema.hrl").
 
 -define(GET_SETTING(Key),
 		begin
 			H = utils:mnesia_read(setting, Key),
 			H#setting.value
-		end
-		).
+		end).
 
-%% Represents the coefficient of friction of new slick tyres 
-%% with rain == 0
+% Length of a segment in meters.
+-define(SEGMENT_LENGTH, 5).
 
-%% http://www.roymech.co.uk/Useful_Tables/Tribology/co_of_frict.htm#coef TODO
+% Time in milliseconds a car needs to move to the adjacent lane.
+-define(LANE_CHANGE_TIME, 500).
+
+% Coefficient of friction of new slick tyres when rain == 0.
+% TODO: http://www.roymech.co.uk/Useful_Tables/Tribology/co_of_frict.htm#coef
 -define(FRICTION_BASE, 10).
+
+-define(FUEL_SPECIFIC_GRAVITY, 0.7).
 
 -define(G, 9.81).
 
-%% TODO Fix value
--define(FUEL_SPECIFIC_GRAVITY, 0.7).
-
-%% TODO Fix value
+% TODO: Speed limit in the pitlane.
 -define(PIT_SPEED_LIM, 42).
 
-%% TODO Fix the value
+% TODO Fix the value
 -define(TYRES_CHANGE, 5000).
--define(TIME_PER_L, 83). %% 12 l/s
 
+-define(TIME_PER_L, 83). % 12 l/s
+
+% Maximum capacity of a car's fuel tank (in liters).
 -define(TANK_DIM, 120).
 
--define(L_PER_SGM, 0.0042)
+-define(L_PER_SGM, 0.0042).
