f4d936849a42e41483d53b6662e08ef59ee146b6
Davide Pesavento <davidepesa@gmail.com>
2009-10-03 15:12:18 +0200

    [event_dispatcher] team_backend wants retire, not surpass notifications.

--- src/event_dispatcher.erl
+++ src/event_dispatcher.erl
@@ -23,9 +23,9 @@
 
 -define(PITSTOP_OBS, [debug_log_backend, race_info_backend]).
 -define(CHRONO_OBS, [debug_log_backend, team_backend, race_info_backend]).
--define(SURPASS_OBS, [debug_log_backend, team_backend, race_info_backend]).
--define(RETIRE_OBS, [debug_log_backend, race_info_backend]).
--define(WEATHER_OBS, [debug_log_backend, weather_backend, team_backend, race_info_backend]).
+-define(SURPASS_OBS, [debug_log_backend, race_info_backend]).
+-define(RETIRE_OBS, [debug_log_backend, team_backend, race_info_backend]).
+-define(WEATHER_OBS, [debug_log_backend, team_backend, race_info_backend, weather_backend]).
 
 -record(state, {}).
 
