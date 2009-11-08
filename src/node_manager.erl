9471850e1a7cf9cc728227b14a677ca1e2abf467
Davide Pesavento <davidepesa@gmail.com>
2009-11-08 17:07:18 +0100

    [node_manager] New largely incomplete module.

---
+++ src/node_manager.erl
@@ -0,0 +1,111 @@
+-module(node_manager).
+
+-behaviour(gen_server).
+
+%% External exports
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1,
+		 handle_call/3,
+		 handle_cast/2,
+		 handle_info/2,
+		 terminate/2,
+		 code_change/3]).
+
+-include("common.hrl").
+
+-record(state, {}).
+
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+start_link() ->
+	gen_server:start_link(?LOCAL_NAME, ?MODULE, [], []).
+
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+	% FIXME: remove hardcoded stuff
+	Hosts = [localhost],
+	net_adm:world_list(Hosts),
+	global:sync(),
+	bootstrap_server:add_node([{scheduler,1},
+							   {event_dispatcher,1},
+							   {team,10},
+							   {car,20},
+							   {weather,1}]),
+	{ok, #state{}}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call(todo, _From, State) ->
+	{reply, ok, State};
+
+handle_call(Msg, From, State) ->
+	?WARN({"unhandled call", Msg, "from", From}),
+	{noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(Msg, State) ->
+	?WARN({"unhandled cast", Msg}),
+	{noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(Info, State) ->
+	?WARN({"unhandled info", Info}),
+	{noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+	ok.
+
+%% --------------------------------------------------------------------
+%% Function: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+
+%% --------------------------------------------------------------------
+%% Internal functions
+%% --------------------------------------------------------------------
