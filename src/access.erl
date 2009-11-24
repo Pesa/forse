c1175e057b2aee9f362ecee9211f84c6d27bd2ce
Davide Pesavento <davidepesa@gmail.com>
2009-11-25 00:05:05 +0100

    [access] Remove trailing whitespaces.

--- src/access.erl
+++ src/access.erl
@@ -21,17 +21,16 @@ check_move(Pilot, Sgm, EnterLane, ExitLane, Pit)
 	Type = Sgm#segment.type,
 	Time = Type == intermediate orelse Type == finish_line,
 	Abs = if
-			   EnterLane == -1 ->
-				   [-2, -1, MinL];
-			   EnterLane == -2 ->
-				   [-2, -1];
-			   EnterLane =< MinL ->
-				   [-1, EnterLane, EnterLane + 1];
-			   true ->
-				   lists:seq(EnterLane - 1, EnterLane + 1)
-		   end,
+			  EnterLane == -1 ->
+				  [-2, -1, MinL];
+			  EnterLane == -2 ->
+				  [-2, -1];
+			  EnterLane =< MinL ->
+				  [-1, EnterLane, EnterLane + 1];
+			  true ->
+				  lists:seq(EnterLane - 1, EnterLane + 1)
+		  end,
 	Unreachable = not lists:member(ExitLane, Abs),
-
 	
 	if
 		% Check if ExitLane exists in this segment
@@ -39,23 +38,23 @@ check_move(Pilot, Sgm, EnterLane, ExitLane, Pit)
 		ExitLane < -2;
 		ExitLane > -1 andalso ExitLane < MinL;
 		Type == normal andalso ExitLane < MinL;
-		ExitLane == -2 andalso (Type == pre_pitlane orelse 
-								Type == post_pitlane orelse 
+		ExitLane == -2 andalso (Type == pre_pitlane orelse
+								Type == post_pitlane orelse
 								Type == pitlane);
 		% Deny lane changes in chrono segments
 		Time andalso ExitLane /= EnterLane;
 		%abs(ExitLane - EnterLane) > 1; TODO
-		% Deny access to pit area when not in need of pitstop
+		% Deny access to pit area when not in need of a pitstop
 		Type == pre_pitlane andalso ExitLane == -1 andalso not Pit;
-		% Separates pit area from main track
-		(Type == pitlane orelse Type == pitstop) andalso EnterLane > 0 andalso ExitLane < 0;
+		% Separate pit area from main track
+		(Type == pitlane orelse Type == pitstop)
+			andalso EnterLane > 0 andalso ExitLane < 0;
 		% pre/post pitlane rules
-		(Type == pre_pitlane orelse Type == post_pitlane) 
-		  andalso EnterLane < 0 andalso ExitLane > 0;
-		% Check if a car crosses more than a lane
+		(Type == pre_pitlane orelse Type == post_pitlane)
+			andalso EnterLane < 0 andalso ExitLane > 0;
+		% Check if a car crosses more than one lane
 		Unreachable ->
 			{fail, 'access denied'};
-		
 		% Check if it's team's own pits
 		Type == pitstop andalso EnterLane == -1 ->
 			T = utils:mnesia_read(car_type, Pilot#pilot.team),
@@ -68,4 +67,3 @@ check_move(Pilot, Sgm, EnterLane, ExitLane, Pit)
 			end;
 		true -> go
 	end.
-
