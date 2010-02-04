470fb4ccd1c1b4a4856df22b8091ac24615c33f8
Davide Pesavento <davidepesa@gmail.com>
2010-02-04 16:49:53 +0100

    [race_info/Car] Added hovering effect and tooltip.

--- src/race_info/Car.py
+++ src/race_info/Car.py
@@ -1,6 +1,6 @@
 from PyQt4.Qt import Qt
 from PyQt4.QtCore import QRectF
-from PyQt4.QtGui import QGraphicsItem, QFont, QPen
+from PyQt4.QtGui import QGraphicsItem, QGraphicsColorizeEffect, QFont, QPen
 
 carSize = 32
 
@@ -8,16 +8,22 @@ class Car(QGraphicsItem):
 
     def __init__(self, track, id, startPos=0, pitLane=False):
         QGraphicsItem.__init__(self)
+        self.setAcceptHoverEvents(True)
         self.setCacheMode(QGraphicsItem.DeviceCoordinateCache)
         self.setOpacity(0.7)
         self.setZValue(4)
         self._track = track
         self._id = id
-        self.updatePos(startPos, pitLane)
+        self._effect = QGraphicsColorizeEffect()
+        self._effect.setColor(Qt.darkMagenta)
+        self._effect.setEnabled(False)
         self._font = QFont()
         self._font.setPointSize(16)
         self._pen = QPen(Qt.black, carSize, Qt.SolidLine, Qt.RoundCap)
         self._rect = QRectF(-carSize / 2, -carSize / 2, carSize, carSize)
+        self.setGraphicsEffect(self._effect)
+        self.setToolTip("Car " + str(self._id))
+        self.updatePos(startPos, pitLane)
         self._translateToNewPos()
 
     def _translateToNewPos(self):
@@ -30,6 +36,16 @@ class Car(QGraphicsItem):
 
     def boundingRect(self):
         return self._rect
+
+    def hoverEnterEvent(self, event):
+        self.graphicsEffect().setEnabled(True)
+        self.setOpacity(1.0)
+        QGraphicsItem.hoverEnterEvent(self, event)
+
+    def hoverLeaveEvent(self, event):
+        self.graphicsEffect().setEnabled(False)
+        self.setOpacity(0.7)
+        QGraphicsItem.hoverLeaveEvent(self, event)
 
     def paint(self, painter, _option, _widget):
         painter.setPen(self._pen)
