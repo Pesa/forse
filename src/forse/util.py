d5dee16f3f20c0f64440af7b5e9c83f455940e01
Davide Pesavento <davidepesa@gmail.com>
2010-02-20 21:28:25 +0100

    [util] Added support for Qt signals.

--- src/forse/util.py
+++ src/forse/util.py
@@ -1,6 +1,6 @@
 import hashlib, os, qt4reactor, random, sys, twotp
 from twotp.term import Atom
-from PyQt4.QtCore import QTimer
+from PyQt4.QtCore import QTimer, pyqtSignal
 from PyQt4.QtGui import QApplication
 
 
@@ -8,6 +8,10 @@ __all__ = ['atomToBool', 'NodeApplication', 'SubscriberApplication']
 
 
 def atomToBool(atom):
+    """
+    Translates an Erlang atom to a Python boolean value.
+    Raises an exception if the conversion fails.
+    """
     if not isinstance(atom, Atom):
         raise TypeError(str(atom) + " is not an atom.")
     if atom.text == "true":
@@ -44,6 +48,9 @@ class NodeApplication(QApplication):
     node, making easier to perform remote operations on Erlang nodes.
     """
 
+    rpcError = pyqtSignal()
+    rpcResult = pyqtSignal(str)
+
     def __init__(self, appName):
         QApplication.__init__(self, sys.argv)
         qt4reactor.install()
@@ -55,31 +62,40 @@ class NodeApplication(QApplication):
         if not self.__nameServer:
             raise ValueError("environment variable FORSE_NS is not defined.")
         self._nodeName = twotp.buildNodeName(appName + "_" + self.__generateRandomHash())
-        self._process = twotp.Process(self._nodeName, self.__cookie)
+        self.__process = twotp.Process(self._nodeName, self.__cookie)
         QTimer.singleShot(0, self.__startup)
 
     def rpc(self, mod, fun, *args):
         """
         Executes C{mod:fun(args)} as a remote procedure call.
         """
-        d = self._process.callRemote(self.__nameServer, mod, fun, *args)
+        d = self._rpc(mod, fun, *args)
         d.addCallback(self.__rpcCB)
-        return d
+        d.addErrback(self.__rpcEB)
+
+    def _rpc(self, mod, fun, *args):
+        return self.__process.callRemote(self.__nameServer, mod, fun, *args)
+
+    def _registerModule(self, name, module):
+        self.__process.registerModule(name, module)
 
     def __generateRandomHash(self, length=8):
         return hashlib.sha1(str(random.random())).hexdigest()[:length]
 
     def __rpcCB(self, result):
         if isinstance(result, Atom):
-            return result.text
+            self.rpcResult.emit(result.text)
         else:
-            return result
+            raise TypeError(str(result) + " is not supported.")
+
+    def __rpcEB(self, _error):
+        self.rpcError.emit()
 
     def __startup(self):
         from twisted.internet import reactor
         reactor.runReturn()
-        self._process.register(self._appName)
-        self._process.listen()
+        self.__process.register(self._appName)
+        self.__process.listen()
         self._startupHook()
 
     def _startupHook(self):
@@ -92,6 +108,9 @@ class SubscriberApplication(NodeApplication):
     service. After subscribing, events are automatically forwarded to
     a set of application-defined handlers.
     """
+
+    subscribed = pyqtSignal()
+    subscriptionError = pyqtSignal()
 
     def __init__(self, appName):
         NodeApplication.__init__(self, appName)
@@ -108,13 +127,12 @@ class SubscriberApplication(NodeApplication):
         """
         cbargs = [Atom(self._nodeName), Atom(self._appName), Atom("handleMessage")]
         callback = Atom("callback"), Atom("rpc"), Atom("call"), cbargs
-        d = self.rpc("event_dispatcher", "subscribe", Atom(self._appName), callback)
+        d = self._rpc("event_dispatcher", "subscribe", Atom(self._appName), callback)
         d.addCallback(self.__subscribeCB)
         d.addErrback(self.__subscribeEB)
-        return d
 
     def _startupHook(self):
-        self._process.registerModule(self._appName, self.__proxy)
+        self._registerModule(self._appName, self.__proxy)
         self.subscribe()
 
     def __retrySubscription(self):
@@ -123,14 +141,12 @@ class SubscriberApplication(NodeApplication):
             self.__retryDelay += 1
 
     def __subscribeCB(self, result):
-        if result == "ok":
+        if isinstance(result, Atom) and result.text == "ok":
             self.__retryDelay = 1
+            self.subscribed.emit()
         else:
-            # TODO: notify GUI
-            print "Subscription failed:", result
-        return result
+            self.subscriptionError.emit()
 
-    def __subscribeEB(self, error):
-        # TODO: notify GUI
-        print "subscribeEB:", error
+    def __subscribeEB(self, _error):
+        self.subscriptionError.emit()
         self.__retrySubscription()
