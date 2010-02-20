a7e857b1ae463a07b8e60cfd3aac06fbb4bdfd88
Davide Pesavento <davidepesa@gmail.com>
2010-02-20 14:33:24 +0100

    No need to use sed in GUI start script; add control panel wrapper.

--- start_gui.sh
+++ start_gui.sh
@@ -3,6 +3,6 @@
 [ -z ${FORSE_COOKIE} ] && export FORSE_COOKIE="42"
 [ -z ${FORSE_NS} ] && export FORSE_NS="foo@$(hostname)"
 
-gui="$(basename $0 | sed 's:^start_\(.*\)\.sh$:\1:')"
+gui="$(basename $0)"
 
-PYTHONPATH="src/forse" python -m ${gui}.Main
+PYTHONPATH="src/forse" python -m ${gui#start_}.Main
