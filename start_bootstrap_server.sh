dcdbf1328a3b176a4c50ca7aed09cfd0eb824d9d
Davide Pesavento <davidepesa@gmail.com>
2010-03-04 00:03:59 +0100

    Deleted obsolete stuff.

--- start_bootstrap_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ERL_LIBS="." erl \
-	-config forse \
-	-setcookie "${FORSE_COOKIE:-42}" \
-	-sname "${1:-foo}@$(hostname)" \
-	-run bootstrap_server
--- start_worker_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ERL_LIBS="." erl \
-	-config forse \
-	-setcookie "${FORSE_COOKIE:-42}" \
-	-sname "${1:-bar}@$(hostname)" \
-	-run node_manager start_link
