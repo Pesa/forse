438dadb154059dc562a8b5681f71fed8229436ab
Davide Pesavento <davidepesa@gmail.com>
2010-03-10 00:23:17 +0100

    [RaceInfoWindow] Allow starting and pausing the simulation through this GUI.

--- src/forse/race_info/RaceInfoWindow.py
+++ src/forse/race_info/RaceInfoWindow.py
@@ -1,6 +1,7 @@
 from PyQt4.QtGui import QMainWindow
 from PositionsModel import PositionsModel
 from Ui_RaceInfoWindow import Ui_RaceInfoWindow
+from remote import Scheduler
 
 
 class RaceInfoWindow(QMainWindow, Ui_RaceInfoWindow):
@@ -8,5 +9,32 @@ class RaceInfoWindow(QMainWindow, Ui_RaceInfoWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self._switchButtonToStart()
         self._posModel = PositionsModel()
         self.positionsView.setModel(self._posModel)
+
+    def pauseSimulation(self):
+        self.startpauseButton.setEnabled(False)
+        self.startpauseButton.clicked.disconnect()
+        Scheduler.pauseSimulation(self._switchButtonToStart)
+
+    def startSimulation(self):
+        self.startpauseButton.setEnabled(False)
+        self.startpauseButton.clicked.disconnect()
+        Scheduler.startSimulation(self._switchButtonToPause)
+
+    def _switchButtonToPause(self, reply="ok"):
+        if reply == "ok":
+            self.startpauseButton.clicked.connect(self.pauseSimulation)
+            self.startpauseButton.setText("Pause simulation")
+            self.startpauseButton.setEnabled(True)
+        else:
+            self.statusBar.showMessage(str(reply), 10000)
+
+    def _switchButtonToStart(self, reply="ok"):
+        if reply == "ok":
+            self.startpauseButton.clicked.connect(self.startSimulation)
+            self.startpauseButton.setText("Start simulation")
+            self.startpauseButton.setEnabled(True)
+        else:
+            self.statusBar.showMessage(str(reply), 10000)
