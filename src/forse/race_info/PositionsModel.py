Pesa/forse

commit 307e775c1a752367138cf35f6d1e3b1f34c8e4cb

[race_info] 'id' is a Python built-in, don't redefine it.

src/forse/race_info/PositionsModel.py
@@ -55,18 +55,18 @@ class PositionsModel(QAbstractTableModel):
         return QVariant()
 
     def _standingsInit(self, standings):
-        self.__positions = dict([(pos, id) for id, pos in standings])
+        self.__positions = dict([(pos, pilot) for pilot, pos in standings])
         self.reset()
 
     def _standingsUpdate(self, standings):
-        for id, newpos in standings:
+        for pilot, newpos in standings:
             for oldpos, pilot in self.__positions.iteritems():
-                if pilot == id:
+                if pilot == pilot:
                     if newpos > oldpos:
-                        PilotInfo.get(id).setIcon(self.__arrowDown)
+                        PilotInfo.get(pilot).setIcon(self.__arrowDown)
                     elif newpos < oldpos:
-                        PilotInfo.get(id).setIcon(self.__arrowUp)
+                        PilotInfo.get(pilot).setIcon(self.__arrowUp)
                     break
-        for id, newpos in standings:
-            self.__positions[newpos] = id
+        for pilot, newpos in standings:
+            self.__positions[newpos] = pilot
         self.reset()

src/forse/race_info/Car.py
@@ -11,14 +11,16 @@ _carSize = 32
 
 class Car(QGraphicsItem):
 
-    def __init__(self, track, id, startPos=0, pitLane=False):
+    def __init__(self, track, carId, startPos=0, pitLane=False):
         QGraphicsItem.__init__(self)
         self.setAcceptHoverEvents(True)
         self.setCacheMode(QGraphicsItem.DeviceCoordinateCache)
         self.setOpacity(0.7)
         self.setZValue(4)
         self._track = track
-        self._id = id
+        self._id = carId
+        self._position = startPos
+        self._pit = pitLane
         self._effect = QGraphicsColorizeEffect()
         self._effect.setColor(Qt.darkMagenta)
         self._effect.setEnabled(False)
@@ -28,7 +30,6 @@ class Car(QGraphicsItem):
         self._rect = QRectF(-_carSize / 2, -_carSize / 2, _carSize, _carSize)
         self.refreshToolTip()
         self.setGraphicsEffect(self._effect)
-        self.updatePos(startPos, pitLane)
         self._translateToNewPos()
 
     def advance(self, phase):

src/forse/race_info/PilotInfo.py
@@ -50,19 +50,19 @@ class PilotInfo(object):
 
     @classmethod
     def _handleCarsState(cls, states):
-        for id, state in states:
-            if id not in cls.__info:
-                cls.__info[id] = PilotInfo()
-            cls.__info[id]._state = state.text
+        for pilot, state in states:
+            if pilot not in cls.__info:
+                cls.__info[pilot] = PilotInfo()
+            cls.__info[pilot]._state = state.text
         cls._refresh()
 
     @classmethod
     def _handleNames(cls, names):
-        for id, name, teamName in names:
-            if id not in cls.__info:
-                cls.__info[id] = PilotInfo()
-            cls.__info[id]._name = QString.fromUtf8(listToString(name))
-            cls.__info[id]._teamName = QString.fromUtf8(listToString(teamName))
+        for pilot, name, teamName in names:
+            if pilot not in cls.__info:
+                cls.__info[pilot] = PilotInfo()
+            cls.__info[pilot]._name = QString.fromUtf8(listToString(name))
+            cls.__info[pilot]._teamName = QString.fromUtf8(listToString(teamName))
         cls._refresh()
 
     def _timerExpired(self):

src/forse/race_info/TrackView.py
@@ -37,10 +37,10 @@ class TrackView(QGraphicsView):
         self._refitScene()
 
     def _initCars(self, cars):
-        for id, pos, pit in cars:
-            car = Car(self._track, id, pos, atomToBool(pit))
-            self._cars[id] = car
-            self._scene.addItem(car)
+        for car, pos, pit in cars:
+            c = Car(self._track, car, pos, atomToBool(pit))
+            self._cars[car] = c
+            self._scene.addItem(c)
 
     def _initTrack(self, track):
         self._track = Track(track)
@@ -48,8 +48,8 @@ class TrackView(QGraphicsView):
         self._refitScene()
 
     def _moveCar(self, value):
-        id, pos, pit = value
-        self._cars[id].updatePos(pos, atomToBool(pit))
+        car, pos, pit = value
+        self._cars[car].updatePos(pos, atomToBool(pit))
 
     def _refitScene(self):
         self.fitInView(self.sceneRect(), Qt.KeepAspectRatio)

commit 47d3866380621ad50c7696619fc4985b4f5517e8

[PositionsModel] Fixed re-use of the same identifier for different things.
Regression introduced in commit 307e775c1a752367138cf35f6d1e3b1f34c8e4cb.

src/forse/race_info/PositionsModel.py
@@ -60,8 +60,8 @@ class PositionsModel(QAbstractTableModel):
 
     def _standingsUpdate(self, standings):
         for pilot, newpos in standings:
-            for oldpos, pilot in self.__positions.iteritems():
-                if pilot == pilot:
+            for oldpos, oldpilot in self.__positions.iteritems():
+                if oldpilot == pilot:
                     if newpos > oldpos:
                         PilotInfo.get(pilot).setIcon(self.__arrowDown)
                     elif newpos < oldpos: