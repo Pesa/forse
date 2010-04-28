478e35ee69a77174cf1309e66e101ca3780d5b8c
Davide Pesavento <davidepesa@gmail.com>
2010-04-29 00:30:30 +0200

    [weather_backend] Most functionality is now implemented.
    ...and maybe it works too...

--- src/weather_backend.erl
+++ src/weather_backend.erl
@@ -15,7 +15,12 @@
 
 -include("common.hrl").
 
--record(state, {subscribers	= []	:: [#subscriber{}]}).
+-type sect_id()	:: non_neg_integer().
+
+-record(state, {subscribers	= []	:: [#subscriber{}],
+				map			= []	:: [{sgm_id(), sect_id()}],
+				sectors		= []	:: [tuple()],
+				weather		= []	:: [{sect_id(), rain_amount()}]}).
 
 
 %% ====================================================================
@@ -64,20 +69,56 @@ handle_call(_Request, _From, State) ->
 %%          {stop, Reason, State}            (terminate/2 is called)
 %% --------------------------------------------------------------------
 handle_cast({subscribe, S}, State) when is_record(S, subscriber) ->
-	NewSubs = event_dispatcher:add_subscriber(S, State#state.subscribers, []),
+	List = [{sectors, State#state.sectors},
+			{weather, State#state.weather}],
+	NewSubs = event_dispatcher:add_subscriber(S, State#state.subscribers, List),
 	{noreply, State#state{subscribers = NewSubs}};
 
+handle_cast(#config_notif{app = track, config = Config}, State) ->
+	{sectors_map, SectorsMap} = lists:keyfind(sectors_map, 1, Config),
+	Map = lists:map(fun({Sect, {_First, Last}}) ->
+							{Last, Sect}
+					end, SectorsMap),
+	
+	{sectors, Sectors} = lists:keyfind(sectors, 1, Config),
+	S = race_info_backend:preprocess_sectors(Sectors),
+	Subs1 = event_dispatcher:notify_init({sectors, S}, State#state.subscribers),
+	
+	ExtractRain = fun({straight, _, _, _, _, Rain}, {W, Id}) ->
+						  {[{Id, Rain} | W], Id + 1};
+					 ({left, _, _, _, _, _, Rain}, {W, Id}) ->
+						  {[{Id, Rain} | W], Id + 1};
+					 ({right, _, _, _, _, _, Rain}, {W, Id}) ->
+						  {[{Id, Rain} | W], Id + 1};
+					 (_, Acc) ->
+						  Acc
+				  end,
+	{Weather, _} = lists:foldl(ExtractRain, {[], 0}, Sectors),
+	Subs2 = event_dispatcher:notify_init({weather, Weather}, Subs1),
+	
+	{noreply, State#state{subscribers = Subs2,
+						  map = lists:keysort(1, Map),
+						  sectors = S,
+						  weather = Weather}};
+
 handle_cast(Msg, State) when is_record(Msg, config_notif) ->
-	%TODO elaborare i dati ricevuti
+	% ignore config_notif from other apps
 	{noreply, State};
 
 handle_cast(Msg, State) when is_record(Msg, race_notif) ->
 	%TODO elaborare i dati ricevuti
 	{noreply, State};
 
-handle_cast(Msg, State) when is_record(Msg, weather_notif) ->
-	%TODO elaborare i dati ricevuti
-	{noreply, State}.
+handle_cast(#weather_notif{changes = Changes}, State) ->
+	SortedChanges = lists:keysort(#weather_change.segment, Changes),
+	SectChanges = sgms_to_sectors(SortedChanges, State#state.map),
+	Apply = fun({Sect, Rain}, Weather) ->
+					lists:keyreplace(Sect, 1, Weather, {Sect, Rain})
+			end,
+	NewWeather = lists:foldl(Apply, State#state.weather, SectChanges),
+	Subs = event_dispatcher:notify_update({weather, SectChanges}, State#state.subscribers),
+	{noreply, State#state{subscribers = Subs,
+						  weather = NewWeather}}.
 
 %% --------------------------------------------------------------------
 %% Function: handle_info/2
@@ -109,3 +150,19 @@ code_change(_OldVsn, State, _Extra) ->
 %% --------------------------------------------------------------------
 %% Internal functions
 %% --------------------------------------------------------------------
+
+-spec sgms_to_sectors([#weather_change{}], [{sgm_id(), sect_id()}]) ->
+		[{sect_id(), rain_amount()}].
+
+sgms_to_sectors([], _) ->
+	[];
+sgms_to_sectors(_, []) ->
+	[];
+sgms_to_sectors([#weather_change{segment = Sgm} = H | T], [{UpperBound, Sect} | Map])
+  when Sgm =< UpperBound ->
+	Rest = lists:dropwhile(fun(#weather_change{segment = S}) ->
+								   S =< UpperBound
+						   end, T),
+	[{Sect, H#weather_change.new_weather} | sgms_to_sectors(Rest, Map)];
+sgms_to_sectors(Changes, [_ | Tail]) ->
+	sgms_to_sectors(Changes, Tail).
