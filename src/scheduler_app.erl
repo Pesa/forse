dd646de0a39a66b6da67e5be7e50f9644a55028e
Davide Pesavento <davidepesa@gmail.com>
2010-05-27 15:28:28 +0200

    [scheduler_app] Send appropriate #race_notif when shutting down.

--- src/scheduler_app.erl
+++ src/scheduler_app.erl
@@ -4,8 +4,10 @@
 
 -export([start/2, stop/1]).
 
+-include("common.hrl").
+
 start(_Type, StartArgs) ->
 	scheduler_sup:start_link(StartArgs).
 
 stop(_State) ->
-	ok.
+	event_dispatcher:notify(#race_notif{event = terminated}).
